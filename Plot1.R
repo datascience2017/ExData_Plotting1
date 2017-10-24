336fa126f09fe6f2057d16575982555d822b214d
datascience2017 <rini.cba@gmail.com>
2017-10-24 14:06:40 -0400

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,14 +1,10 @@
-##Read the text file from working directory and then print the data
-mydata<- read.table("household_power_consumption.txt",header=TRUE,sep=";")
-
-#Formatting date
-mydata$Date <- as.Date(mydata$Date,format("%d/%m/%Y"))
-head(mydata,3)
+##Read the text file from working directory
+mydata<- read.table("household_power_consumption.txt",header=TRUE,sep=";",stringsAsFactors=FALSE, dec=".")
+#Formatting date 
 #Read from dates 2007-02-01 and 2007-02-02, hence subsetting data
-mydata2 <- subset(mydata,mydata$Date >="2007-02-01" & mydata$Date <="2007-02-01")
-head(mydata2,3)
-Global_active_power<- as.numeric(mydata2$Global_active_power)
-Global_active_power <- Global_active_power/1000
+mydata2 <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+globalActivePower <- as.numeric(mydata2$Global_active_power)
+head(globalActivePower)
 png("plot1.png", width=480, height=480)
-hist(Global_active_power,col="red",xlab= "GlobalActive Power(kilowatts)", breaks=15,las=1,ylim = c(0,1200), xlim=c(0,4), main="Global Active Power")
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
 dev.off()
