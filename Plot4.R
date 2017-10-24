626d19099d3ef44e2d1933370329d15ac953619c
datascience2017 <rini.cba@gmail.com>
2017-10-24 16:16:44 -0400

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -1,5 +1,4 @@
 ##Read the text file from working directory and then print the data
-##Read the text file from working directory and then print the data
 mydata<- read.table("household_power_consumption.txt",header=TRUE,sep=";",stringsAsFactors=FALSE, dec=".")
 #subsetting data
 mydata3 <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
@@ -13,10 +12,9 @@ Sub_metering_2 <- as.numeric(mydata3$Sub_metering_2)
 Sub_metering_3 <- as.numeric(mydata3$Sub_metering_3)
 Global_reactive_power <- as.numeric(mydata3$Global_reactive_power)
 Voltage <- as.numeric(mydata3$Voltage)
-
+# plotting the graph area
 png("plot4.png", width=480, height=480)
 par(mfrow = c(2, 2)) 
-
 
 #Plot1  - Global Active Power  vs Day
 plot(Combineddatetime, globalActivePower, type="l", ylab="GlobalActive Power", xlab="")
@@ -26,7 +24,6 @@ lines(Combineddatetime, globalActivePower, type="l")
 #Plot#2 - Voltage vs date and time
 plot(Combineddatetime, Voltage, type="l", ylab="Voltage", xlab="datetime")
 
-
 #Plot#3- Energy Submetering vs day
 plot(Combineddatetime, Sub_metering_1, type="l", ylab="Energy Submetering", xlab="")
 lines(Combineddatetime, Sub_metering_2, type="l", col="red")
@@ -34,7 +31,6 @@ lines(Combineddatetime, Sub_metering_3, type="l", col="blue")
 
 #Plot#4- Global Reactive Power(y) vs datatime(x)
 plot(Combineddatetime, Global_reactive_power, type="l", ylab="Global_reactive_power", xlab="datetime")
-
 dev.off()
 
 
