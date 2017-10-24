8ff900fe5b43d674454a16b2601af48919fa049b
datascience2017 <rini.cba@gmail.com>
2017-10-24 15:00:41 -0400

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -1,23 +1,14 @@
 ##Read the text file from working directory and then print the data
-mydata<- read.table("household_power_consumption.txt",header=TRUE,sep=";")
-
-
+mydata<- read.table("household_power_consumption.txt",header=TRUE,sep=";",stringsAsFactors=FALSE, dec=".")
+#subsetting data
 mydata3 <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
-
-Combineddatetime <- strptime(paste(mydata3$Date, mydata3$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
-
-
+#formatting data
+Combineddatetime <- strptime(paste(mydata3$Date, mydata3$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
 #converting from factor to numeric
-globalActivePower <- as.numeric(subSetData$Global_active_power)
-Sub_metering_1 <- as.numeric(mydata3$Sub_metering_1)
-Sub_metering_2 <- as.numeric(mydata3$Sub_metering_2)
-Sub_metering_3 <- as.numeric(mydata3$Sub_metering_3)
-
+globalActivePower <- as.numeric(mydata3$Global_active_power)
 png("plot2.png", width=480, height=480)
 plot(Combineddatetime, globalActivePower, type="l", ylab="GlobalActive Power(kilowatts)", xlab="")
 lines(Combineddatetime, globalActivePower, type="l")
 lines(Combineddatetime, globalActivePower, type="l")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
 dev.off()
-
-
