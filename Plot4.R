b3f2b24eb5eb05affd22e41a11d79e8fb8db94a9
datascience2017 <rini.cba@gmail.com>
2017-10-24 16:15:56 -0400

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -1,11 +1,10 @@
 ##Read the text file from working directory and then print the data
-mydata<- read.table("household_power_consumption.txt",header=TRUE,sep=";")
-
-
+##Read the text file from working directory and then print the data
+mydata<- read.table("household_power_consumption.txt",header=TRUE,sep=";",stringsAsFactors=FALSE, dec=".")
+#subsetting data
 mydata3 <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
-
-Combineddatetime <- strptime(paste(mydata3$Date, mydata3$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
-
+#formatting data
+Combineddatetime <- strptime(paste(mydata3$Date, mydata3$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
 
 #converting from factor to numeric
 globalActivePower <- as.numeric(subSetData$Global_active_power)
@@ -19,30 +18,24 @@ png("plot4.png", width=480, height=480)
 par(mfrow = c(2, 2)) 
 
 
-#Plot1    
-plot(Combineddatetime, globalActivePower, type="l", ylab="GlobalActive Power(kilowatts)", xlab="")
+#Plot1  - Global Active Power  vs Day
+plot(Combineddatetime, globalActivePower, type="l", ylab="GlobalActive Power", xlab="")
 lines(Combineddatetime, globalActivePower, type="l")
 lines(Combineddatetime, globalActivePower, type="l")
 
-#Plot#3- voltage(y) vs datatime(x)
-#png("plot_sample.png", width=480, height=480)
-#par(mar=c(1,1,1,1))
+#Plot#2 - Voltage vs date and time
 plot(Combineddatetime, Voltage, type="l", ylab="Voltage", xlab="datetime")
 
 
-
-
-
-#Plot#2
-plot(Combineddatetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
-lines(Combineddatetime, subMetering2, type="l", col="red")
-lines(Combineddatetime, subMetering3, type="l", col="blue")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+#Plot#3- Energy Submetering vs day
+plot(Combineddatetime, Sub_metering_1, type="l", ylab="Energy Submetering", xlab="")
+lines(Combineddatetime, Sub_metering_2, type="l", col="red")
+lines(Combineddatetime, Sub_metering_3, type="l", col="blue")
 
 #Plot#4- Global Reactive Power(y) vs datatime(x)
 plot(Combineddatetime, Global_reactive_power, type="l", ylab="Global_reactive_power", xlab="datetime")
 
-
 dev.off()
+
 
 
